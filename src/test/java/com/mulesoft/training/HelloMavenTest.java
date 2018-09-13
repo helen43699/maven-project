b8cd853272267906296e98af3b9e9b34b457acc1
Ying Alvarado <ying.alvarado@perficient.com>
2018-09-13 13:31:36 -0700

    was able to deploy to cloudhub on command line: mvn clean package mule:deploy -Dpwd=<my anypoint password>

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,27 @@
+package com.mulesoft.training;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.api.MuleEvent;
+import static org.junit.Assert.assertEquals;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+    @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Override
+    protected String getConfigResources() {
+        return "maven-project.xml";
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+	    MuleEvent event = runFlow("retrieveFlights");
+	    String contentType = event.getMessage().getOutboundProperty("Content-Type");
+	    assertEquals("application/json", contentType);
+    }
+
+}
